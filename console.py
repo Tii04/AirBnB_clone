5ca67972bb9ff39fa72fafa684421750f6da51cc
Tii04 <Tii04@github.com>
2022-11-28 23:11:09 +0200

    pycodestyle compliance

--- console.py
+++ console.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+""" This is the creation of the console"""
 
 import cmd
 
@@ -7,15 +8,15 @@ class HBNBCommand(cmd.Cmd):
     """ Simple command-line processor"""
 
     prompt = '(hbnb) '
-    
+
     def do_EOF(self, line):
         """ Cleanly exits program"""
-        
+
         return True
 
     def do_quit(self, line):
         """Quit command to exit the program"""
-    
+
         return True
 
 
