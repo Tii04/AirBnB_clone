1f6905e32851abfd09693e412fdac39406a9f414
Tii04 <Tii04@github.com>
2022-11-28 12:07:09 +0200

    Task 3

---
+++ models/base_model.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""This class defines all attributes for other classes"""
+
+
+from uuid import uuid4
+from datetime import datetime
+import json
+
+
+class BaseModel():
+    """ This class defines all attributes for other classes that
+        inherit it."""
+
+    def __init__(self):
+
+        self.id = str(uuid4())
+        self.created_at = self.updated_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """ Specifies the format of printing"""
+
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                                      self.id, self.__dict__))
+
+    def save(self):
+        """ updates the public instance attribute
+        updated_at with the current datetime"""
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """  returns a dictionary containing all
+        keys/values of __dict__ of the instance"""
+
+        dictt = {}
+        for key, item in self.__dict__.items():
+            if key in ['created_at', 'updated_at']:
+                dictt[key] = item
+
+        dictt['__class__'] = self.__class__.__name__
+        dictt['created_at'] = self.created_at.isoformat()
+        dictt['updated_at'] = self.updated_at.isoformat()
+
+        return dictt
